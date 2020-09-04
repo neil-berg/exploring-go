7f4df5085c4c355b3524c8cd51f7f1e0808bb85b
Neil Berg <neil.berg14@gmail.com>
2020-09-03 22:54:28 -0700

    importing explorations

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/neil-berg/exploring-go/morestrings"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
 }
